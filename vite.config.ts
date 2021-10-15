445709a344cdd9ee598cf081578097497384df62
ikeohachidi <ikeohachidi@gmail.com>
2021-10-15 22:02:19 +0100

    feat: alias @

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const path = require('path');
+// import path from 'path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.join(__dirname, 'src')
+      }
+    ]
+  }
 })
