18d0fb44c34c21c9767d90948ce8e624c0490dc1
ikeohachidi <ikeohachidi@gmail.com>
2021-10-19 23:30:59 +0100

    refactor: allow more flexibility with scoped slots

--- src/components/or-accordion/or-accordion-body.vue
+++ src/components/or-accordion/or-accordion-body.vue
@@ -1,13 +1,62 @@
 <template>
-    <slot></slot>
+    <p class="or-accordion-title" @click="toggleAccordion">
+        <span>
+            <slot name="title" :title="title">
+                {{ title }}
+            </slot>
+        </span>
+        <span>
+            <slot name="collapse" :isCollapsed="isComponentActive">
+                <i class="ri-subtract-line" v-if="isComponentActive"></i>
+                <i class="ri-add-line" v-else></i>
+            </slot>
+        </span>
+    </p>
+    <div class="or-accordion-body" :class="{'active': isComponentActive ? true : false}">
+        <div>
+            <slot>
+            </slot>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
+import { useAttrs, computed } from "@vue/runtime-core";
 
 const props = defineProps<{
     title: string;
-    description?: string;
 }>();
 
+const attrs = useAttrs();
+
+const isComponentActive = computed(() => attrs._isActive)
+
+const emit = defineEmits<{
+    (event: 'selectionChange', value: number): void
+}>()
+
+const toggleAccordion = () => {
+    emit('selectionChange', attrs._index as number)
+}
 
 </script>
+
+<style lang="scss" scoped>
+.or-accordion-title {
+    @apply border-b border-gray-200 p-3 cursor-pointer;
+    @apply flex items-center justify-between; 
+}
+
+.or-accordion-body {
+    @apply overflow-y-hidden transition-all  bg-gray-50;
+    @apply max-h-0;
+
+    &.active {
+        @apply max-h-96;
+    }
+
+    > div {
+        @apply p-3;
+    }
+}
+</style>
--- src/components/or-accordion/or-accordion.vue
+++ src/components/or-accordion/or-accordion.vue
@@ -1,18 +1,12 @@
 <template>
     <ul class="or-accordion-container" ref="accordionContainer">
         <li v-for="(slot, slotIndex) in defaultSlots" :key="slotIndex" class="or-accordion">
-            <p class="or-accordion-title" @click="toggleAccordionSection(slotIndex)">
-                <span>{{ slot.props.title }}</span>
-                <span>
-                    <i class="ri-subtract-line" v-if="activeSlotIndex === slotIndex"></i>
-                    <i class="ri-add-line" v-else></i>
-                </span>
-            </p>
-            <div class="or-accordion-body">
-                <div>
-                    <component :is="slot"/>
-                </div>
-            </div>
+            <component 
+                :is="slot" 
+                @selectionChange="updateActiveSection" 
+                :_isActive="activeSection === slotIndex" 
+                :_index="slotIndex"
+            />
         </li>
     </ul>
 </template>
@@ -22,26 +16,15 @@ import { ref, useSlots, computed, VNode } from "vue";
 
 const slots = useSlots();
 
-const activeSlotIndex = ref(-1);
-const accordionContainer = ref<HTMLUListElement>();
+const activeSection = ref(-1);
 
-const toggleAccordionSection = (slotIndex: number): void => {
-    const accordionBodyElements = Array.from(accordionContainer.value!.querySelectorAll(".or-accordion-body"));
-
-    if (slotIndex === activeSlotIndex.value) {
-        accordionBodyElements[slotIndex].classList.remove('active');
-        activeSlotIndex.value = -1;
-
-        return;
+const updateActiveSection = (newActiveSection: number) => {
+    if (activeSection.value === newActiveSection) {
+        activeSection.value = -1;
+        return
     }
 
-    accordionBodyElements.forEach(element => {
-        element.classList.remove('active');
-    })
-
-    accordionBodyElements[slotIndex].classList.add('active');
-
-    activeSlotIndex.value = slotIndex;
+    activeSection.value = newActiveSection;
 }
 
 const defaultSlots = computed<VNode[]>(() => {
@@ -50,22 +33,3 @@ const defaultSlots = computed<VNode[]>(() => {
     return []
 });
 </script>
-
-<style lang="scss" scoped>
-.or-accordion-title {
-    @apply border-b border-gray-200 p-3 cursor-pointer;
-    @apply flex items-center justify-between; 
-}
-
-.or-accordion-body {
-    @apply overflow-y-hidden max-h-0 transition-all  bg-gray-50;
-
-    &.active {
-        @apply max-h-96;
-    }
-
-    > div {
-        @apply p-3;
-    }
-}
-</style>
