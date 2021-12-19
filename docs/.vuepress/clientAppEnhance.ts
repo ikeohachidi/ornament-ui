d606a24d3168df2441d7a0ff10ef30cd490a3735
ikeohachidi <ikeohachidi@gmail.com>
2021-12-19 01:11:13 +0100

    feat: add kwik-css import

--- docs/.vuepress/clientAppEnhance.ts
+++ docs/.vuepress/clientAppEnhance.ts
@@ -1,7 +1,8 @@
 import { defineClientAppEnhance } from '@vuepress/client';
 
-import '@/main.css';
+import 'kwik-css';
 import 'remixicon/fonts/remixicon.css';
+import '@/main.css';
 
 import OrPlugin from '../../src/plugin';
 
