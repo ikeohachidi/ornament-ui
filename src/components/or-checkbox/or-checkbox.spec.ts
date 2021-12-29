2cd66d9fac678fb19e9148b25c366d04c3b44645
ikeohachidi <ikeohachidi@gmail.com>
2021-12-29 02:23:16 +0100

    test: or-checkbox

---
+++ src/components/or-checkbox/or-checkbox.spec.ts
@@ -0,0 +1,21 @@
+import { mount } from "@vue/test-utils";
+import { OrCheckbox } from "@/plugin";
+
+const wrapper = mount(OrCheckbox, {
+	props: {
+		modelValue: ["orange", "pepper"],
+		value: "orange"
+	},
+})
+
+describe("OrCheckbox", () => {
+	it("doesn't display switch in default mode", () => {
+		expect(wrapper.find('.or-switch').exists()).toBeFalsy();
+	})
+
+	it('emits update:modelvalue event on change and takes action', async () => {
+		await wrapper.find('.or-checkbox-label').trigger('click');
+
+		expect(wrapper.emitted()).toHaveProperty('update:modelValue');
+	})
+})
