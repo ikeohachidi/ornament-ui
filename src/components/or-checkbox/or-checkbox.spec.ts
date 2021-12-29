45a8b35377a28877f5dd3db2e977ddd47dfec9ec
ikeohachidi <ikeohachidi@gmail.com>
2021-12-29 15:52:44 +0100

    test: update or-checkbox test

--- src/components/or-checkbox/or-checkbox.spec.ts
+++ src/components/or-checkbox/or-checkbox.spec.ts
@@ -7,16 +7,24 @@ const wrapper = mount(OrCheckbox, {
 		modelValue: ["orange", "pepper"],
 		value: "orange"
 	},
+	slots: {
+		default: "fruits"
+	}
 })
 
 describe("OrCheckbox", () => {
+	it('renders slot content', () => {
+		expect(wrapper.html()).toContain('fruits');
+	})
+
 	it("doesn't display switch in default mode", () => {
 		expect(wrapper.find('.or-switch').exists()).toBeFalsy();
 	})
 
-	it('emits update:modelvalue event on change and takes action', async () => {
+	it('emits update event', async () => {
 		await wrapper.find('.or-checkbox-label').trigger('click');
+		const emitted = wrapper.emitted()
 
-		expect(wrapper.emitted()).toHaveProperty('update:modelValue');
+		expect(emitted).toHaveProperty('update:modelValue');
 	})
 })
