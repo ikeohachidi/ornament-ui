a52d1a61ef011be2753039c3c951ee078b7ea362
ikeohachidi <ikeohachidi@gmail.com>
2021-12-29 22:55:15 +0100

    test: or-list

---
+++ src/components/or-list/or-list.spec.ts
@@ -0,0 +1,52 @@
+import { mount } from '@vue/test-utils';
+import { OrList, OrListItem } from '@/plugin';
+
+const items = [
+	{
+		title: 'veggie',
+		description: 'vegetable'
+	},
+	{
+		title: 'fruit',
+		description: 'oranges'
+	}
+];
+
+const factory = (props = {}, slots = {}) => {
+	return mount(OrList, {
+		components: {
+			'or-list-item': OrListItem
+		},
+		props: {
+			items,
+			...props
+		},
+		slots
+	})
+}
+
+describe('OrList', () => {
+	it ('should display items', () => {
+		const wrapper = factory();
+
+		items.forEach(item => {
+			expect(wrapper.html()).toContain(item.title)
+			expect(wrapper.html()).toContain(item.description)
+		})
+	})
+
+	it('should discard props items rendering when slots is used', () => {
+		const wrapper = factory({}, {
+			default: `
+				<or-list-item>Socrates</or-list-item>
+			`
+		});
+
+		items.forEach(item => {
+			expect(wrapper.html()).not.toContain(item.title)
+			expect(wrapper.html()).not.toContain(item.description)
+		})
+
+		expect(wrapper.html()).toContain('Socrates')
+	})
+})
