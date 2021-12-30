48c10e389a75f3e9f814d36a6ac178a8669b7ca7
ikeohachidi <ikeohachidi@gmail.com>
2021-12-30 18:29:13 +0100

    test(or-dropdown): add test cases

--- src/components/or-dropdown/or-dropdown.spec.ts
+++ src/components/or-dropdown/or-dropdown.spec.ts
@@ -3,24 +3,29 @@ import { OrCheckbox, OrChips, OrDropdown, OrInput } from "@/plugin";
 
 const options = ['orange', 'pepper'];
 
-const wrapper = mount(OrDropdown, {
-	components: {
-		'or-input': OrInput,
-		'or-chips': OrChips,
-		'or-checkbox': OrCheckbox,
-	},
-	props: {
-		options,
-		hasFilter: true,
-		placeholder: 'Food'
-	},
-	data: function() {
-		return {
-			filterTerm: ''
-		}
-	}
-})
+const factory = (props = {}, slots = {}) => {
+	return mount(OrDropdown, {
+		components: {
+			'or-input': OrInput,
+			'or-chips': OrChips,
+			'or-checkbox': OrCheckbox,
+		},
+		props: {
+			options,
+			hasFilter: true,
+			placeholder: 'Food',
+			...props
+		},
+		data: function() {
+			return {
+				filterTerm: ''
+			}
+		},
+		slots,
+	})
+}
 
+const wrapper = factory();
 const filterInputEl =  wrapper.findComponent({ ref: 'filterInput' });
 const optionsTriggerEl = wrapper.find('[data-testid="options-trigger"]');
 const optionsListEl = wrapper.find('.or-dropdown-list');
@@ -55,6 +60,29 @@ describe('OrDropdown', () => {
 	it('hides options when list root nodes are not clicked', async () => {
 		await optionsTriggerEl.trigger('click');
 		expect(optionsListEl.classes()).not.toContain('show');
+	})
+
+	it('emits correct correct update value', async () => {
+		await wrapper.find('.or-dropdown-item').trigger('click');
+		expect(wrapper.emitted('update:modelValue')![0]).toEqual(['orange']);
+	})
+
+	it('displays options with optionLabel prop when its provided', () => {
+		const options = [{ firstname: 'chidi', lastname: 'ikeoha' }, { firstname: 'kish', lastname: 'ikeoha' }]
+		const wrapper = factory({ options, optionLabel: 'firstname' })
+
+		options.forEach(option => {
+			expect(wrapper.html()).toContain(option.firstname);
+			expect(wrapper.html()).not.toContain(option.lastname);
+		})
+	})
+
+	it('sets value to optionValue prop when its provided', async () => {
+		const options = [{ firstname: 'chidi', lastname: 'ikeoha' }, { firstname: 'kish', lastname: 'ikeoha' }]
+		const wrapper = factory({ options, optionValue: 'firstname' })
+
+		await wrapper.find('.or-dropdown-item').trigger('click');
+		expect(wrapper.emitted('update:modelValue')![0]).toEqual([options[0].firstname])
 	})
 	// TODO: test for multi selection
 })
