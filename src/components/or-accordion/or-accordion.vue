8e2f231562c55f1d931e3ad959a69f095e502922
ikeohachidi <ikeohachidi@gmail.com>
2022-01-22 12:30:09 +0100

    fix: remove list style dots

--- src/components/or-accordion/or-accordion.vue
+++ src/components/or-accordion/or-accordion.vue
@@ -1,14 +1,14 @@
 <template>
-    <ul class="or-accordion-container" ref="accordionContainer">
-        <li v-for="(slot, slotIndex) in defaultSlots" :key="slotIndex" class="or-accordion">
-            <component 
-                :is="slot" 
-                @selectionChange="updateActiveSection" 
-                :_isActive="activeSection === slotIndex" 
-                :_index="slotIndex"
-            />
-        </li>
-    </ul>
+	<ul class="or-accordion-container" ref="accordionContainer">
+		<li v-for="(slot, slotIndex) in defaultSlots" :key="slotIndex" class="or-accordion">
+			<component 
+				:is="slot" 
+				@selectionChange="updateActiveSection" 
+				:_isActive="activeSection === slotIndex" 
+				:_index="slotIndex"
+			/>
+		</li>
+	</ul>
 </template>
 
 <script setup lang="ts">
@@ -19,17 +19,23 @@ const slots = useSlots();
 const activeSection = ref(-1);
 
 const updateActiveSection = (newActiveSection: number) => {
-    if (activeSection.value === newActiveSection) {
-        activeSection.value = -1;
-        return
-    }
+	if (activeSection.value === newActiveSection) {
+		activeSection.value = -1;
+		return
+	}
 
-    activeSection.value = newActiveSection;
+	activeSection.value = newActiveSection;
 }
 
 const defaultSlots = computed<VNode[]>(() => {
-    if (slots.default) return slots.default() as VNode[];
+	if (slots.default) return slots.default() as VNode[];
 
-    return []
+	return []
 });
 </script>
+
+<style lang="scss" scoped>
+li {
+	list-style: none;
+}
+</style>
