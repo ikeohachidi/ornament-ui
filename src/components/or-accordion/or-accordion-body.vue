501226e5cdcce9f8ff3b37184b84cb747bb6dcb5
ikeohachidi <ikeohachidi@gmail.com>
2022-01-22 12:34:25 +0100

    fix: janky visibility animation

--- src/components/or-accordion/or-accordion-body.vue
+++ src/components/or-accordion/or-accordion-body.vue
@@ -1,30 +1,30 @@
 <template>
-    <p data-testid="title" class="or-accordion-title m-0 p-2 flex center jc-between" @click="toggleAccordion">
-        <span>
-            <slot name="title" :title="title">
-                {{ title }}
-            </slot>
-        </span>
-        <span>
-            <slot name="collapse" :isCollapsed="isComponentActive">
-                <i class="ri-subtract-line" v-if="isComponentActive"></i>
-                <i class="ri-add-line" v-else></i>
-            </slot>
-        </span>
-    </p>
-    <div class="or-accordion-body" :class="{'active': isComponentActive ? true : false}">
-        <div class="p-3">
-            <slot>
-            </slot>
-        </div>
-    </div>
+	<p data-testid="title" class="or-accordion-title m-0 p-2 flex center jc-between" @click="toggleAccordion">
+		<span>
+			<slot name="title" :title="title">
+				{{ title }}
+			</slot>
+		</span>
+		<span>
+			<slot name="collapse" :isCollapsed="isComponentActive">
+				<i class="ri-subtract-line" v-if="isComponentActive"></i>
+				<i class="ri-add-line" v-else></i>
+			</slot>
+		</span>
+	</p>
+	<div class="or-accordion-body" :class="[isComponentActive ? 'active' : 'inactive']">
+		<div class="p-3">
+			<slot>
+			</slot>
+		</div>
+	</div>
 </template>
 
 <script setup lang="ts">
 import { useAttrs, computed } from "vue";
 
 const props = defineProps<{
-    title: string;
+	title: string;
 }>();
 
 const attrs = useAttrs();
@@ -32,29 +32,33 @@ const attrs = useAttrs();
 const isComponentActive = computed(() => attrs._isActive)
 
 const emit = defineEmits<{
-    (event: 'selectionChange', value: number): void
+	(event: 'selectionChange', value: number): void
 }>()
 
 const toggleAccordion = () => {
-    emit('selectionChange', attrs._index as number)
+	emit('selectionChange', attrs._index as number)
 }
 
 </script>
 
 <style lang="scss" scoped>
 .or-accordion-title {
-    cursor: pointer;
-    border-bottom: 1px solid var(--color-gray-1);
+	cursor: pointer;
+	border-bottom: 1px solid var(--color-gray-1);
 }
 
 .or-accordion-body {
-    overflow-y: auto;
-    max-height: 0;
-    background: var(--color-gray-2);
-    transition: .2s;
+	overflow-y: auto;
+	background: var(--color-gray-2);
 
-    &.active {
-        max-height: 200px;
-    }
+	&.inactive {
+		max-height: 0;
+		transition: .2s;
+	}
+
+	&.active {
+		max-height: 200px;
+		transition: .5s;
+	}
 }
 </style>
