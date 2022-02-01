a39b471a99a48e47e0cc4c6eb748ec5a3320eebf
ikeohachidi <ikeohachidi@gmail.com>
2022-02-01 09:00:01 +0100

    test: add or-overlay test

---
+++ src/components/or-overlay/or-overlay.spec.ts
@@ -0,0 +1,26 @@
+import { mount } from '@vue/test-utils';
+import { OrOverlay } from '@/plugin';
+
+const factory = (props = {}, slots = {}) => {
+	return mount(OrOverlay, { 
+		attachTo: document.body,
+		props,
+		slots,
+	});
+}
+
+describe('OrOverlay', () => {
+	it ('renders backdrop', () => {
+		const component = factory()
+		const backdropEl = component.find('[data-testid="backdrop"]');
+		expect(backdropEl).toBeTruthy();
+	})
+
+	it ('doesn\'t render backdrop', () => {
+		const component = factory({
+			hasBackdrop: false 
+		})
+		const backdropEl = component.find('[data-testid="backdrop"]');
+		expect(backdropEl.exists()).toBeFalsy();
+	})
+})
