3f29be34808a699aab8cd756d020106d5f18169e
ikeohachidi <ikeohachidi@gmail.com>
2022-03-19 19:50:49 +0100

    fix(docs): unresolved menu-node component

--- docs/.vuepress/clientAppEnhance.ts
+++ docs/.vuepress/clientAppEnhance.ts
@@ -3,9 +3,11 @@ import { defineClientAppEnhance } from '@vuepress/client';
 import 'kwik-css';
 import 'remixicon/fonts/remixicon.css';
 import '@/main.scss';
+import { MenuNode } from '../../src/components/or-vertical-menu';
 
 import OrPlugin from '../../src/plugin';
 
 export default defineClientAppEnhance(({ app }) => {
     app.use(OrPlugin);
+    app.component('menu-node', MenuNode);
 })
