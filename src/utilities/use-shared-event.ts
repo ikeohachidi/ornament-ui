c3101eed448c5d8b8ca03d5c7ddf0922f9bec1e2
ikeohachidi <ikeohachidi@gmail.com>
2022-05-05 23:36:26 +0100

    refactor: remove redundant pubsub implementation

--- src/utilities/use-shared-event.ts
+++ src/utilities/use-shared-event.ts
@@ -1,46 +1,3 @@
-const events: Record<string, {
-	push: (value: unknown) => void;
-	listen: (callback: Function) => void;
-	flush: () => void;
-	_listeners: Function[];
-}> = {};
-
-
-/**
- * useEvent is a simple pubsub implementation
- * @param event the name of the event to listen to
- */
-const useEvent = (event: string) => {
-	if (!event) {
-		throw new Error("Attempting to create nameless event");
-		
-	}
-
-	if ((event in events) === false) {
-		events[event] = {
-			_listeners: [],
-			push: function(value: unknown) {
-				this._listeners.forEach(_listener => {
-					_listener(value);
-				})
-			},
-			listen: function(callback: Function) {
-				this._listeners.push(callback);
-			},
-			flush: function(): void {
-				this._listeners = [];
-			}
-		}
-	}
-
-	const e = events[event];
-	return {
-		push: (value: unknown) => e.push(value),
-		listen: (callback: Function) => e.listen(callback),
-		flush: () => e.flush()
-	}
-}
-
 type onCallback = (value?: any) => void;
 type Emitter = {
 	events: {[event: string]: onCallback[]};
@@ -93,5 +50,3 @@ const emitter: Emitter = {
 export {
 	emitter
 }
-
-export default useEvent;
