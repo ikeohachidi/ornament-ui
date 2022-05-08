74b90251cec54b1d9d4c73b674d6442cc6ff664f
ikeohachidi <ikeohachidi@gmail.com>
2022-05-08 13:09:22 +0100

    feat: add tests

---
+++ src/components/or-list/or-list-item/or-list-item.spec.ts
@@ -0,0 +1,53 @@
+import { mount } from '@vue/test-utils';
+import { OrListItem, Events } from '..';
+import OrCheckbox from '@/components/or-checkbox';
+
+const factory = (props = {}, slots = {}) => {
+	return mount(OrListItem, {
+		attachTo: document.body,
+		components: {
+			OrCheckbox
+		},
+		props,
+		slots
+	})
+}
+
+describe('OrListItem.vue', () => {
+	it ('displays checkbox dependent on checkable prop value', async () => {
+		const component = factory({
+			checkable: false
+		})
+
+		expect(component.find('[data-testid="checkbox"]').exists()).toBeFalsy();
+
+		await component.setProps({ checkable: true })
+		expect(component.find('[data-testid="checkbox"]').exists()).toBeTruthy();
+	})
+
+	it ('displays default slot values', () => {
+		const text = 'Hello world';
+		const component = factory({}, {
+			default: text 
+		})
+
+		expect(component.html()).toContain(text)
+	})
+
+	it('emits events on checkbox toggle', async () => {
+		const value = 'hello world';
+		const component = factory({
+			checkable: true,
+			value
+		})
+
+		const checkbox = component.find('[data-testid="checkbox"]');
+		await checkbox.trigger('click')
+
+		expect(component.emitted()).toHaveProperty(Events.ITEM_CHECK);
+
+		await checkbox.trigger('click');
+
+		expect(component.emitted()).toHaveProperty(Events.ITEM_UNCHECK);
+	})
+})
--- src/components/or-list/or-list.spec.ts
+++ src/components/or-list/or-list.spec.ts
@@ -1,52 +1,46 @@
 import { mount } from '@vue/test-utils';
+import { h } from 'vue';
 import { OrList, OrListItem } from '.';
-
-const items = [
-	{
-		title: 'veggie',
-		description: 'vegetable'
-	},
-	{
-		title: 'fruit',
-		description: 'oranges'
-	}
-];
 
 const factory = (props = {}, slots = {}) => {
 	return mount(OrList, {
+		attachTo: document.body,
 		components: {
-			'or-list-item': OrListItem
+			OrListItem
 		},
-		props: {
-			items,
-			...props
-		},
+		props,
 		slots
 	})
 }
 
 describe('OrList', () => {
-	it ('should display items', () => {
-		const wrapper = factory();
-
-		items.forEach(item => {
-			expect(wrapper.html()).toContain(item.title)
-			expect(wrapper.html()).toContain(item.description)
-		})
-	})
-
-	it('should discard props items rendering when slots is used', () => {
-		const wrapper = factory({}, {
-			default: `
-				<or-list-item>Socrates</or-list-item>
-			`
+	it('should not display items if not wrapped in or-list-item', () => {
+		const text = 'hello world';
+		const component = factory({}, {
+			default: text
 		});
 
-		items.forEach(item => {
-			expect(wrapper.html()).not.toContain(item.title)
-			expect(wrapper.html()).not.toContain(item.description)
+		expect(component.html()).not.toContain(text)
+	})
+
+	it('should display items in or-list-item', () => {
+		const text = 'hello world';
+		const component = factory({}, {
+			default: h(OrListItem, text) 
 		})
 
-		expect(wrapper.html()).toContain('Socrates')
+		expect(component.html()).toContain(text)
+	})
+
+	it('should display header and footer slot content', () => {
+		const header = 'head';
+		const footer = 'tail';
+		const component = factory({}, {
+			header,
+			footer
+		})
+
+		expect(component.html()).toContain(header);
+		expect(component.html()).toContain(footer);
 	})
 })
