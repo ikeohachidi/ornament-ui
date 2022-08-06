5ac71276c019bc62fec3dbc0b50fbf28d55764d6
ikeohachidi <ikeohachidi@gmail.com>
2022-08-06 12:41:44 +0100

    feat: add step slider behaviour

--- src/components/or-slider/or-slider.vue
+++ src/components/or-slider/or-slider.vue
@@ -1,6 +1,12 @@
 <template>
 	<div class="or-slider" ref="thumbWrapperEl">
-		<div class="or-slider__thumb" @mousedown="onMouseDown" ref="thumbEl"></div>
+		<div class="or-slider__thumbs">
+			<div
+				class="or-slider__thumb"
+				@mousedown="onMouseDown"
+				ref="thumbEl"
+			></div>
+		</div>
 	</div>
 </template>
 
@@ -9,12 +15,14 @@ import { onMounted, ref, computed, unref, watch } from 'vue';
 
 const props = withDefaults(defineProps<{
 	modelValue: number,
-	min: number,
-	max: number,
+	min?: number,
+	max?: number,
+	steps?: number
 }>(), {
 	modelValue: 0,
 	min: 0,
-	max: 20
+	max: 20,
+	steps: 5 
 });
 
 const emit = defineEmits<{
@@ -26,11 +34,25 @@ let mousePosition = 0;
 let thumbPosition = ref(0);
 
 const thumbWrapperEl = ref<HTMLDivElement>();
+
 const thumbWrapperElWidth = computed(() => {
 	if (thumbWrapperEl.value) {
 		return parseInt(getComputedStyle(thumbWrapperEl.value).width.replace('px', ''));
 	}
 	return 0;
+});
+
+const singleStepPx = computed(() => {
+	return thumbWrapperElWidth.value / props.steps;
+});
+const allStepsPx = computed(() => {
+	const steps: number[] = [0];
+
+	for (let i = 1; i <= props.steps; i++) {
+		steps.push(singleStepPx.value * i);
+	}
+
+	return steps;
 });
 
 const updateModelValue = () => {
@@ -40,41 +62,82 @@ const updateModelValue = () => {
 	emit('update:modelValue', thumbPosPercOfWrapper);
 }
 
+const updateModelValueStep = (step: number) => {
+	emit('update:modelValue', step);
+}
+
 const thumbEl = ref<HTMLDivElement>();
+const currentThumbPosition = ref(0);
+const globalTracker = ref(0);
 const setThumbPosition = (position: number, x: number): void => {
 	window.requestAnimationFrame(() => {
 		if (thumbEl.value) {
-			const marginLeft = thumbEl.value.style.marginLeft;
+			const leftPosition = thumbEl.value.style.left;
 			const unit = 'px';
 			// expecting that it'll have % at the end of it
-			let marginLeftValue = Number(marginLeft.replace(unit, ''));
+			currentThumbPosition.value = Number(leftPosition.replace(unit, ''))
 
 			// the difference between the last registered mouse position
 			// and the present position. This is better than just adding
 			// and subtracting 1 to the margin because depending on the
 			// mouse move speed it may be greater than 1.
 			const mouseDiff = x - mousePosition;
-			const newThumbPosition = marginLeftValue + mouseDiff;
+			const newThumbPosition = currentThumbPosition.value + mouseDiff;
 
+			if (props.steps > 0) {
+				if (globalTracker.value === 0 && currentThumbPosition.value === 0) {
+					globalTracker.value += 2;
+				}
+				else if (globalTracker.value === 0) {
+					globalTracker.value = currentThumbPosition.value;
+				}
+				else {
+					globalTracker.value += mouseDiff;
+				}
 
+				const threshold = 5;
+
+				for (let i = 0; i < allStepsPx.value.length; i++) {
+					const step = allStepsPx.value[i];
+					const from = step - threshold;
+					const to = step + threshold;
+
+					if (globalTracker.value >= from && globalTracker.value <= to) {
+						if (props.modelValue !== i) {
+							thumbEl.value.style.left = step + unit;
+							// thumbPosition.value = step;
+							updateModelValueStep(i)
+						}
+					}
+				}
+
+				document.addEventListener('mouseup', () => {
+					console.log('global tracker resetting:', globalTracker.value)
+					globalTracker.value = 0;
+				});
+
+				mousePosition = x;
+				return;
+			}
+
+			// smooth slider
 			if (newThumbPosition > 0 && newThumbPosition < thumbWrapperElWidth.value) {
-				if (marginLeftValue === 0) {
-					marginLeftValue++;
+				if (currentThumbPosition.value === 0) {
+					currentThumbPosition.value++;
 				} 
 				// assume we are beginning from a mouseup
 				// in order to avoid large space jank
 				else if (mouseDiff > mousePosition) {
-					marginLeftValue++;
+					currentThumbPosition.value++;
 				}
 				else {
-					marginLeftValue += mouseDiff;
+					currentThumbPosition.value += mouseDiff;
 				}
 			}
 
-			thumbEl.value.style.marginLeft = marginLeftValue + unit;
-			thumbPosition.value = marginLeftValue;
+			thumbEl.value.style.left = unref(currentThumbPosition) + unit;
+			thumbPosition.value = unref(currentThumbPosition);
 			mousePosition = x;
-
 			updateModelValue();
 		}
 	})
@@ -93,15 +156,13 @@ const onMouseDown = (event: MouseEvent): void => {
 	if (!thumbEl.value) return;
 
 	document.body.addEventListener('mousemove', onMouseMove);
-	document.body.addEventListener('mouseup', () => {
-		mousePosition = 0;
-	})
 	removeListeners(document.body);
 }
 
 const removeListeners = (element: HTMLElement): void => {
 	document.addEventListener('mouseup', () => {
 		element.removeEventListener('mousemove', onMouseMove);
+		mousePosition = 0;
 	});
 }
 </script>
@@ -114,13 +175,15 @@ $size: 40px;
 	width: 100%;
 	background-color: red;
 	box-sizing: border-box;
-	
+	position: relative;
+
 	&__thumb {
 		box-sizing: border-box;
 		height: $size;
 		width: $size;
 		background-color: blue;
-		transition: 0.01s;
+		position: absolute;
+		left: 0;
 	}
 }
 </style>
